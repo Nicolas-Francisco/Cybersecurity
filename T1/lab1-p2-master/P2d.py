0b6ade7b704111984038abc31ef1e95ceab790f6
JavierLavados <javierlavadosjilbert@gmail.com>
2022-04-21 07:36:03 -0400

    se avanza en la d

--- T1/lab1-p2-master/P2d.py
+++ T1/lab1-p2-master/P2d.py
@@ -1,35 +1,53 @@
 # P2d - Cree una función que permita descifrar el último carácter del texto cifrado.
 
+from re import I
 import socket
 import utils
 import P2c
 SIZE_BLOCK = 16 # bytes
 
-CONNECTION_ADDR = ("172.17.69.107", 5312)
+SERVER_A= ("172.17.69.107", 5312)
+SERVER_B = ("172.17.69.107", 5313)
 
-if __name__ == "__main__":
-    sock_input, sock_output = utils.create_socket(CONNECTION_ADDR)
-    try:
-        # Read a message from standard input
-        response = input("send a message: ")
-        # You need to use encode() method to send a string as bytes.
-        print("[Client] \"{}\"".format(response))
-        resp = utils.send_message(sock_input, sock_output, response)
-        print("[Server] \"{}\"".format(resp))
-        # Wait for a response and disconnect.
-    except Exception as e:
-        print(e)
-        print("Closing...")
-        input.close()
+mensaje = "Cherry, cerra el orto"
 
-print("modo hacker")
 
-C = bytearray.fromhex(resp)
+sock_input, sock_output = utils.create_socket(SERVER_A)
+try:
+    resp = utils.send_message(sock_input, sock_output, mensaje)
+except Exception as e:
+    print(e)
+    input.close()
 
-def get_last_block(C):
-    return C[-SIZE_BLOCK:]
 
-Cn = get_last_block(C)
-Cn1 = get_last_block(C[0:-SIZE_BLOCK])
-Cn_block1 = C[-1:]
-Cn1_block1 = Cn1[-1:]
+print("Modo hacker")
+#Transformo el cifrado hexagonal en bytes
+C_bytes = utils.hex_to_bytes(resp)
+
+#separo c_bytes en bloques de 16
+C_block = utils.split_blocks(C_bytes, SIZE_BLOCK)
+Cn = C_block[-1] #Obtengo el ultimo bloque
+Cn_menos_1 = C_block[-2]
+
+#Obtengo el ultimo bytes
+Cn_last_byte = Cn[SIZE_BLOCK-1]
+Cn_menos_last_byte = Cn_menos_1[SIZE_BLOCK-1]
+# guardo en una variable  Cn-1
+Mn_menos_1=Cn_menos_1
+# Mn-1[BlockSize-1]=[0x00]
+Mn_menos_1[SIZE_BLOCK-1]=0
+#mientras Dn[blockSize-1]!=[0x00],aumentar Mn-1[BlockSize-1] en 1
+
+i=1
+sock_input, sock_output = utils.create_socket(SERVER_B)
+while i<=256:
+    #Cambio Cn-1 por Mn-1
+    C_block[-2]=Mn_menos_1
+    #Junto los bloques y lo paso a hexagonal
+    C_Modificado=utils.bytes_to_hex(utils.join_blocks(C_block))
+    #Envio el mensaje codificado al servidor B
+    resp2 = utils.send_message(sock_input, sock_output, C_Modificado)
+
+    Mn_menos_1[-1:]=i
+    i+=1
+
