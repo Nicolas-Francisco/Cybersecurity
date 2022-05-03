f0f2f3b847379e9e4c2f1dfe696fd04d6b015740
JavierLavados <javierlavadosjilbert@gmail.com>
2022-05-03 14:58:22 -0400

    aaa

--- T2/P1.py
+++ T2/P1.py
@@ -4,6 +4,7 @@ import time
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding 
 
 CONNECTION_ADDR = ('localhost', 5327)
 # CONNECTION_ADDR = ("172.17.69.107", 5327)
@@ -16,51 +17,53 @@ if __name__ == "__main__":
     sock_output = sock_input.makefile(errors="ignore")
 
     sock_input.bind(CONNECTION_ADDR)
-    sock_input.listen(5)
+    sock_input.listen(1)
 
     while True:
         conn, addr = sock_input.accept()
 
         actual_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-        print("Fecha del log: {}".format(actual_time))
+        
        
         while True:
             try:
-                data = conn.recv(16)
+                data = conn.recv(1024)
 
             except Exception as e:
                 print(e)
                 print("Closing...")
-                input.close()
                 break
 
-            print("Texto en request recibida: {}".format(data.decode()))
+            print("-----------------------------------------------------")
+            print("Fecha del log: {}".format(actual_time))
+            print("Texto en request recibida: {}".format(data.decode()), end="")
             print("IP en request recibida: {}".format(addr[1]))
-            conn.send("Mensaje recibido".encode())
 
-            # Primer gezeo (gzip) el msj
-            print("Largo de respuesta no comprimida: {}".format(len(data)))
-            t=gzip.compress(data)
+            msj = formatMessage.format(data.decode(), SECRET)
+
+            print("Largo de respuesta no comprimida: {}".format(len(msj)))
+            t=gzip.compress(msj.encode())
             print("Largo de respuesta comprimida con gzip: {}".format(len(t)))
- 
-            # Segundo aeseo (AES-CBC) 
+
             backend = default_backend()     # Configuración que la librería pide pero no usa por un problema de diseño
             key = os.urandom(16)            # Llave usada por el cifrador de bloque
             iv = os.urandom(16)             # Vector de inicialización usado por el modo
 
             cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
             encryptor = cipher.encryptor() 
-            ct = encryptor.update(t*16) # Entrega parte de lo encriptado
+
+            padder = padding.PKCS7(algorithms.AES.block_size).padder()
+            t_padded = padder.update(t)
+            t_padded += padder.finalize()
+
+            ct = encryptor.update(t_padded) # Entrega parte de lo encriptado
             ct += encryptor.finalize() # Devuelve todo lo encriptado, en caso de haber quedado datos sin devolver anteriormente
             
-            
-
-            #Por ultimo, hexagoneo (hexstring)
-            msj = formatMessage.format(ct, SECRET)
-            hex_msj = msj.encode('utf-8').hex()
+            hex_msj = ct.hex()
             print("Largo de respuesta comprimida con gzip cifrada: {}".format(len(hex_msj)/2))
+
+            conn.send(hex_msj.encode())
             
-        
 
         conn.close()
-        print('Desconecta3')
+        print('Desconecto')
