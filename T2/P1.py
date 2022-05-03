a41ee63f387f44158d594e15ab47c3f38290f29d
Nicolás García <nicolas.garcia.r@ug.uchile.cl>
2022-05-03 15:05:17 -0400

    se termina la p1

--- T2/P1.py
+++ T2/P1.py
@@ -6,10 +6,13 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding 
 
-CONNECTION_ADDR = ('localhost', 5327)
-# CONNECTION_ADDR = ("172.17.69.107", 5327)
+# CONNECTION_ADDR = ('localhost', 5327)
+CONNECTION_ADDR = ("172.17.69.107", 5327)
 
+# header solicitado
 formatMessage = "GET {} HTTP/1.1\nCookie: secret={}\nHost: cc5325.dcc\n\n"
+
+# mensaje secreto
 SECRET = "H0LAaaa3ST03SUNS3CRE70MUYS3CR3T0"
 
 if __name__ == "__main__":
@@ -21,9 +24,7 @@ if __name__ == "__main__":
 
     while True:
         conn, addr = sock_input.accept()
-
         actual_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-        
        
         while True:
             try:
@@ -34,36 +35,47 @@ if __name__ == "__main__":
                 print("Closing...")
                 break
 
+            # log solicitado
             print("-----------------------------------------------------")
             print("Fecha del log: {}".format(actual_time))
             print("Texto en request recibida: {}".format(data.decode()), end="")
             print("IP en request recibida: {}".format(addr[1]))
 
+            # se dan los datos recibidos por el servidor y el mensaje secreto
+            # al header para proceder con el cifrado
             msj = formatMessage.format(data.decode(), SECRET)
 
             print("Largo de respuesta no comprimida: {}".format(len(msj)))
-            t=gzip.compress(msj.encode())
+
+            # se comprime el mensaje en bytes
+            t = gzip.compress(msj.encode())
             print("Largo de respuesta comprimida con gzip: {}".format(len(t)))
 
             backend = default_backend()     # Configuración que la librería pide pero no usa por un problema de diseño
             key = os.urandom(16)            # Llave usada por el cifrador de bloque
             iv = os.urandom(16)             # Vector de inicialización usado por el modo
 
+            # Se crea el cifrador de bloque AES CBC
             cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
             encryptor = cipher.encryptor() 
 
+            # Se crea un padder para el mensaje, ya que se requiere que tenga multiplo de 16
             padder = padding.PKCS7(algorithms.AES.block_size).padder()
+
+            # Se le aplica el padder al mensaje
             t_padded = padder.update(t)
             t_padded += padder.finalize()
 
-            ct = encryptor.update(t_padded) # Entrega parte de lo encriptado
-            ct += encryptor.finalize() # Devuelve todo lo encriptado, en caso de haber quedado datos sin devolver anteriormente
+            # Se cifra el mensaje con padding
+            ct = encryptor.update(t_padded)     # Entrega parte de lo encriptado
+            ct += encryptor.finalize()          # Devuelve todo lo encriptado
             
+            # Se pasa el mensaje cifrado a hexadecimal
             hex_msj = ct.hex()
             print("Largo de respuesta comprimida con gzip cifrada: {}".format(len(hex_msj)/2))
 
-            conn.send(hex_msj.encode())
-            
+            # Se envia el mensaje cifrado. En este caso no es necesario
+            # conn.send(hex_msj.encode())
 
         conn.close()
         print('Desconecto')
