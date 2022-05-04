123a88d605eded177c10eddcb3017027981ef791
Nicolás García <nicolas.garcia.r@ug.uchile.cl>
2022-05-04 15:43:05 -0400

    se crean todos los métodos, incluido el ataque CRIME

--- T2/P2b.py
+++ T2/P2b.py
@@ -18,29 +18,26 @@ GZIP_X = gzip.compress(X.encode())      # gzip de X
 UNKOWN_START = ''
 UNKOWN_END = ''
 
-def COMPRESSION_ORACLE(DATA):
-    backend = default_backend()  
-    # key y iv definidos pero no explícitos
+def encrypt_aes_cbc(msj):
     key = os.urandom(16)            
     iv = os.urandom(16)            
-
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     encryptor = cipher.encryptor() 
     padder = padding.PKCS7(algorithms.AES.block_size).padder()
-    
-    X = UNKOWN_START + DATA + UNKOWN_END
-    GZIP_X = gzip.compress(X.encode())
-    
-    t_padded = padder.update(GZIP_X) + padder.finalize()
+    t_padded = padder.update(msj) + padder.finalize()
     ENCRYPT_X = encryptor.update(t_padded) + encryptor.finalize()   
+    return ENCRYPT_X
 
-    return len(ENCRYPT_X)
+
+def COMPRESSION_ORACLE(DATA):
+    X = UNKOWN_START + DATA + UNKOWN_END
+    return len(encrypt_aes_cbc(gzip.compress(X.encode())))
 
 
 def ALGORITHM(DATA):
     POSSIBLE = []   # arreglo vacío con todas las respuestas posibles
     y = ''          # y es el conjunto de 5 a 10 carácteres fuera de w
-    UNCOMPRESSED_LENGTH = len(COMPRESSION_ORACLE(KNOWN + y))
+    UNCOMPRESSED_LENGTH = COMPRESSION_ORACLE(KNOWN + y)
     NO_W = ''       # conjunto de caracteres que no están en w
     for c in W:     # recorremos todos los caracteres de w
         BASE_LENGTH = COMPRESSION_ORACLE(KNOWN + NO_W + c)
@@ -70,3 +67,15 @@ def COMPUTE_PADDING(KNOWN):
 
     KNOWN = BASURA + KNOWN
     return BASURA[:-1]
+
+
+def CRIME_ATTACK():
+    # computamos el padding
+    PADDING = COMPUTE_PADDING(KNOWN)
+    # computamos la respuesta
+    RESPONSE = ALGORITHM(KNOWN + PADDING)
+    # computamos el secreto
+    SECRET = ''
+    for r in RESPONSE:
+        SECRET += r[-1]
+    return SECRET
