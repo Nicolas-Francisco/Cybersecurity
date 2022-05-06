23ea715f7a754c13dd523f00cbd109a0bbd95703
JavierLavados <javierlavadosjilbert@gmail.com>
2022-05-06 12:39:21 -0400

    potot

--- T2/P1.py
+++ T2/P1.py
@@ -29,54 +29,64 @@ if __name__ == "__main__":
     while True:
         conn, addr = sock_input.accept()
         actual_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        data_total = " "
        
-        while True:
-            try:
-                data = conn.recv(4096)
+        data_total = conn.recv(4096)
+        #while True:
+         #   try:
+          #      data = conn.recv(16)
+           #     data_total += data.decode()
+#
+ #               if len(data)<16:
+  #                  print("no more")
+   #                 break
 
-            except Exception as e:
-                print(e)
-                print("Closing...")
-                break
+    #        except Exception as e:
+     #           print(e)
+      #          #print("Closing...")
+       #         break
 
-            # log solicitado
-            # print("-----------------------------------------------------")
-            # print("Fecha del log: {}".format(actual_time))
-            print("Texto en request recibida: {}".format(data.decode()))
-            # print("IP en request recibida: {}".format(addr[1]))
+        # log solicitado
+        # print("-----------------------------------------------------")
+        # print("Fecha del log: {}".format(actual_time))
+        print("Texto en request recibida: {}".format(data_total))
+        # print("IP en request recibida: {}".format(addr[1]))
 
-            # se dan los datos recibidos por el servidor y el mensaje secreto
-            # al header para proceder con el cifrado
-            msj = formatMessage.format(data.decode(), SECRET)
+        # se dan los datos recibidos por el servidor y el mensaje secreto
+        # al header para proceder con el cifrado
+        msj = formatMessage.format(data_total, SECRET)
 
-            # print("Largo de respuesta no comprimida: {}".format(len(msj)))
+        print("Largo de respuesta no comprimida: {}".format(len(msj)))
 
-            # se comprime el mensaje en bytes
-            t = gzip.compress(msj.encode())
-            # print("Largo de respuesta comprimida con gzip: {}".format(len(t)))
+        # se comprime el mensaje en bytes
+        t = gzip.compress(msj.encode())
+        print("Largo de respuesta comprimida con gzip: {}".format(len(t)))
 
-            # Se crea el cifrador de bloque AES CBC
-            cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-            encryptor = cipher.encryptor() 
+        # Se crea el cifrador de bloque AES CBC
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+        encryptor = cipher.encryptor() 
 
+        if len(t)%16 !=0:
             # Se crea un padder para el mensaje, ya que se requiere que tenga multiplo de 16
             padder = padding.PKCS7(algorithms.AES.block_size).padder()
 
             # Se le aplica el padder al mensaje
             t_padded = padder.update(t)
             t_padded += padder.finalize()
+        else:
+            t_padded=t
 
-            # print("Largo de respuesta comprimida con gzip padeado: {}".format(len(t_padded)))
-            # Se cifra el mensaje con padding
-            ct = encryptor.update(t_padded)     # Entrega parte de lo encriptado
-            ct += encryptor.finalize()          # Devuelve todo lo encriptado
-            
-            # Se pasa el mensaje cifrado a hexadecimal
-            hex_msj = ct.hex()
-            print("Largo de respuesta comprimida con gzip cifrada: {}".format(len(hex_msj)//2))
+        print("Largo de respuesta comprimida con gzip padeado: {}".format(len(t_padded)))
+        # Se cifra el mensaje con padding
+        ct = encryptor.update(t_padded)     # Entrega parte de lo encriptado
+        ct += encryptor.finalize()          # Devuelve todo lo encriptado
+        
+        # Se pasa el mensaje cifrado a hexadecimal
+        hex_msj = ct.hex()
+        print("Largo de respuesta comprimida con gzip cifrada: {}".format(len(hex_msj)//2))
 
-            # Se envia el mensaje cifrado. En este caso no es necesario
-            conn.send(hex_msj.encode())
+        # Se envia el mensaje cifrado. En este caso no es necesario
+        conn.send(hex_msj.encode())
 
         conn.close()
-        print('Closing Server...')
+        print('Closing conection')
--- T2/P2c.py
+++ T2/P2c.py
@@ -13,61 +13,97 @@ CONNECTION_ADDR = ('localhost', 5327)
 # # mensaje secreto
 # SECRET = "H0LAaaa3ST03SUNS3CRE70MUYS3CR3T0"
 
-def COMPRESSION_ORACLE(socket, MSJ):
+def COMPRESSION_ORACLE(MSJ):
+    #print("COMPRESION_ORACLE")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(CONNECTION_ADDR)
+    data=""
     # enviamos el mensaje para su compresión
-    socket.send(MSJ.encode())
-    return len(socket.recv(4096).decode())
+    s.sendall(MSJ.encode())
+    data_total = ""
+    data_total = s.recv(4096)
+    #while True:
+     #   try:
+      #      data = s.recv(16)
+       #     data_total += data.decode()
+#
+ #           if len(data)<16:
+  #              print("no more")
+   #             break
+
+    #    except Exception as e:
+     #       print(e)
+            #print("Closing...")
+      #      break
+    s.close()
+    return len(data_total)
 
 
-def ALGORITHM(socket, KNOWN):
+def ALGORITHM(KNOWN):
+    #print("ALGORITHM")
+    PADDING = COMPUTE_PADDING(KNOWN + NO_W)
     POSSIBLE = []       # arreglo vacío con todas las respuestas posibles
     for c in W:         # recorremos todos los caracteres de w
-        BASE_LENGTH = COMPRESSION_ORACLE(socket, KNOWN + NO_W + c)
-        C_LENGTH = COMPRESSION_ORACLE(socket, KNOWN + c + NO_W)
+        #print("Primero mayor que segundo gano {}".format(c))
+        BASE_LENGTH = COMPRESSION_ORACLE(PADDING + KNOWN + NO_W + c)
+        #print(BASE_LENGTH)
+        C_LENGTH = COMPRESSION_ORACLE(PADDING + KNOWN + c + NO_W)
+        #print(C_LENGTH)
         if C_LENGTH < BASE_LENGTH:
-            print("a")
             POSSIBLE.append(c)
 
     print("[POSSIBLE] \"{}\"".format(POSSIBLE))
     # RESPONSES contendrá todas las posibles soluciones para SECRET,
     # justo después de KNOWN
-    print(POSSIBLE)
-    RESPONSES = []
-    if POSSIBLE!= []:
-        for p in POSSIBLE:
-            RESPONSES +=ALGORITHM(KNOWN+P)
+    #print(POSSIBLE)
+    #RESPONSES = []
+    #if POSSIBLE!= []:
+    #    for p in POSSIBLE:
+    #        RESPONSES +=ALGORITHM(KNOWN+p)
 
     
-    return RESPONSES
+    return POSSIBLE
 
 
-def COMPUTE_PADDING(socket, KNOWN):
+def COMPUTE_PADDING(KNOWN):
     # computamos un padding para que el largo calce con el largo del bloque
-    BASE_LENGTH = COMPRESSION_ORACLE(socket, KNOWN)
+    BASE_LENGTH = COMPRESSION_ORACLE(KNOWN)
     NEW_LENGTH = BASE_LENGTH
     BASURA = ''
     while NEW_LENGTH == BASE_LENGTH:
-        NEW_LENGTH = COMPRESSION_ORACLE(socket, BASURA + KNOWN)
+        NEW_LENGTH = COMPRESSION_ORACLE(BASURA + KNOWN)
         if NEW_LENGTH <= BASE_LENGTH:
             BASURA += random.choice(W)
-
+    #KNOWN = BASURA + KNOWN
     return BASURA[:-1]
 
 
-def CRIME_ATTACK(socket, IKNOW):
-    SECRET = ''
-    while len(SECRET) < 32:
-        PADDING = COMPUTE_PADDING(socket, IKNOW + SECRET)
-        print("[PADDING] \"{}\"".format(PADDING))
+def CRIME_ATTACK(IKNOW):
+    #SECRET = ''
+    #while len(SECRET) < 32:
+        #PADDING = COMPUTE_PADDING(IKNOW + SECRET)
+       # print("[PADDING] \"{}\"".format(PADDING))
 
-        KNOWN =  PADDING + IKNOW + SECRET
+        #KNOWN =  PADDING + IKNOW + SECRET
 
         # computamos la respuesta
-        RESPONSES = ALGORITHM(socket, KNOWN)
-        print("[RESPONSES] \"{}\"".format(RESPONSES))
+    RESPONSE = ALGORITHM(IKNOW)
+    print("[RESPONSES] \"{}\"".format(RESPONSE))
+
+    left=32
+    
+    while left >0:
+        POSSIBLES=[]
+        for p in RESPONSE:
+            POSSIBLE = ALGORITHM(IKNOW + p)
+            for j in  POSSIBLE:
+                POSSIBLES.append(p+j)
+        RESPONSE=  POSSIBLES
+        left-=1
+        print("RESPONSE {}".format(RESPONSE))
         
-    print("[SECRET] \"{}\"".format(SECRET))
-    return SECRET
+    #print("[SECRET] \"{}\"".format(SECRET))
+    return RESPONSE
 
 
 if __name__ == "__main__":
@@ -76,20 +112,12 @@ if __name__ == "__main__":
     key = os.urandom(16)  
     iv = os.urandom(16)  
     IKNOW = 'Cookie: secret='
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(CONNECTION_ADDR)
+    
     # while True:
 
-    try:
-        print("-----------------------------------------------------")
-        print("-------------------- CRIME ATTACK -------------------")
+    print("-----------------------------------------------------")
+    print("-------------------- CRIME ATTACK -------------------")
 
-        CRIME_ATTACK(s, IKNOW)
+    CRIME_ATTACK(IKNOW)
 
-        print("-----------------------------------------------------")
-    except Exception as e:
-        print(e)
-        print("Closing Client...")
-        input.close()
-        s.close()
-        # break
+    print("-----------------------------------------------------")
